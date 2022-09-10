b73330597251ade4b9ddb1a4fb9fadd60af2780e
BashayerGhazi <112524443+BashayerGhazi@users.noreply.github.com>
2022-09-11 02:08:39 +0300

    done

---
+++ athkar/athkar/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  athkar
+//
+//  Created by Shahad on 10/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    //@State var text = " "
+    @State var num = 0
+    @State var number = 0
+    @State var number1 = 1
+    @State var number2 = 2
+    @State var number3 = 3
+    @State var numberA = 0
+    @State var numberB = 0
+    var body: some View {
+        VStack{
+            HStack{
+                Text("استغفر الله العظيم")
+                ExtractedView(num: $number)
+                
+            }
+            HStack{
+                Text("سبحان الله وبحمده")
+                ExtractedView(num: $numberA)
+            }
+            HStack{
+                Text("سبحان الله العظيم")
+               ExtractedView(num: $numberB)
+            }
+            
+        }
+    }
+}
+
+
+
+struct ExtractedView: View {
+    @Binding var num: Int
+    @State var number1 = 1
+    @State var number2 = 2
+    @State var number3 = 3
+    
+    var body: some View {
+        ZStack{
+            Circle().frame(width: 100, height: 100).foregroundColor(.green)
+            Text("\(num)").font(.largeTitle)
+                .onTapGesture {
+                    num = number1
+                    number1 = number2
+                    number2 = number3
+                }
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
